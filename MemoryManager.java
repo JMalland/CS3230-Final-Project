b498da83d95a2a3ffc8059515062b6c55a953042
Jacob Malland <jacob.malland@gmail.com>
2025-05-10 15:48:31 -0500

    Updated MemoryManager.java   - Fixed best-fit memory block search.   - Unsure how to handle no-block-found.   - Implemented printMemory()

--- MemoryManager.java
+++ MemoryManager.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
  * allocates more RAM to a process than currently free, release the memory in-use, and wait.
  */
 public class MemoryManager {
+    // Number of rows to be displayed
+    private static int ROWS = 10;
+
     // Used a fixed-size array to simulate memory blocks
     private int[] memory;
     private HashMap<Integer, ArrayList<Range>> table;
@@ -29,6 +32,19 @@ public class MemoryManager {
 
         // Release the semaphore lock
         semaphore.signal();
+    }
+
+    /**
+     * Point a block of memory to a process
+     * @param pid The process ID
+     * @param block The memory block
+     */
+    private void setBlock(int pid, Range block) {
+        // Go through each byte in the block
+        for (int i=block.start(); i<block.end(); i++) {
+            // Set the byte held by PID
+            memory[i] = pid;
+        }
     }
 
     /**
@@ -56,41 +72,50 @@ public class MemoryManager {
         Range bestFit = null;
 
         // Go through each block of free memory
-        for (Range block : freeMemory) {
+        for (Range freeBlock : freeMemory) {
             // The block is large enough, and is the smallest block yet found
-            if (bestFit == null || block.size() >= size && block.size() < bestFit.size()) {
+            if (freeBlock.size() >= size && (bestFit == null || freeBlock.size() < bestFit.size())) {
                 // Save the smallest available block
-                bestFit = block;
+                bestFit = freeBlock;
             }
         }
 
-        // The allocation request is too large, so release all held memory and wait for a large enough block
+        // No suitable block was found
+        // Free this processes held memory, and request one large allocation
         if (bestFit == null) {
-            // Release the semaphore lock
-            semaphore.signal();
+            System.out.println("Could not allocate memory.");
+            // // Calculate the total memory needed for this process
+            // int needed = 0;
+            // // Go through each held block and add its size
+            // for (Range heldBlock : table.get(pid)) {
+            //     needed += heldBlock.size();
+            // }
 
-            // Release all memory held by process
-            this.free(pid);
+            // // The needed cumulative memory for this process
+            // size += needed;
 
-            // Wait for a large enough allocation block
-            allocate(pid, size);
+            // // Release the semaphore lock before calling free and waiting
+            // semaphore.signal();
 
-            // Quit the function
-            return;
+            // // Release all memory held by the process
+            // this.free(pid);
         }
 
         // Remove the best-fit block from the table
         // because we will break it into two blocks
         table.get(0).remove(bestFit);
 
-        // Create the smallest block to hold the allocation.
-        Range allocated = new Range(bestFit.start(), bestFit.start() + size);
-
         // Create the allocation list for the process
         table.putIfAbsent(pid, new ArrayList<Range>());
 
-        // Add the allocated block to the table, for the process
+        // Create the smallest block to hold the allocation.
+        Range allocated = new Range(bestFit.start(), bestFit.start() + size);
+
+        // Add the allocated block to the table for the process
         table.get(pid).add(allocated);
+
+        // Change the memory list to reflect the held memory block
+        setBlock(pid, allocated);
         
         // The best fit block will have a hole of free memory left over
         if (bestFit.size() > size) {
@@ -120,48 +145,57 @@ public class MemoryManager {
         // Get the processes memory allocation list
         ArrayList<Range> heldMemory = table.get(pid);
 
-        // Check that the process is holding memory
-        if (heldMemory.size() > 0) {
-            // Remove each memory block from the process 
-            for (int i=0; i<heldMemory.size(); i++) {
-                // Sort the free memory so each block is in order
-                // i.e. (0, 5), (5, 8), (8, 12)
-                freeMemory.sort((a, b) -> Integer.compare(a.start(), b.start()));
+        // The process isn't holding any memory
+        if (heldMemory.size() == 0) {
+            // Release the semaphore lock
+            semaphore.signal();
+            return;
+        }
 
-                // A block of memory (no-longer) held by the process
-                Range heldBlock = heldMemory.remove(i);
-                
-                // Go through each block of free memory and attempt to merge with released held memory
-                for (int j=0; j<freeMemory.size() - 1; j++) {
-                    // A block of free memory
-                    Range freeBlock = freeMemory.get(j);
+        // Remove each memory block from the process 
+        for (int i=0; i<heldMemory.size(); i++) {
+            // Sort the free memory so each block is in order
+            // i.e. (0, 5), (5, 8), (8, 12)
+            freeMemory.sort((a, b) -> Integer.compare(a.start(), b.start()));
 
-                    // No remaining free blocks can be connected to the held block
-                    if (freeBlock.start() > heldBlock.start()) {
-                        break;
-                    }
+            // A block of memory (no-longer) held by the process
+            Range heldBlock = heldMemory.remove(i);
 
-                    // The held memory block is connected to a free memory block
-                    if (freeBlock.end() == heldBlock.start() || heldBlock.end() == freeBlock.start()) {
-                        // Merge the free block and the held block
-                        Range mergedBlock = new Range(Math.min(freeBlock.start(), heldBlock.start()), Math.max(heldBlock.end(), freeBlock.end()));
-                        
-                        // Replace the free memory block with the two blocks merged
-                        freeMemory.set(j, mergedBlock);
+            // Change the memory list to reflect the held memory as freed
+            setBlock(0, heldBlock);
+            
+            // Go through each block of free memory and attempt to merge with released held memory
+            for (int j=0; j<freeMemory.size() - 1; j++) {
+                // A block of free memory
+                Range freeBlock = freeMemory.get(j);
 
-                        // Set held memory block to null, to indicate freed
-                        heldBlock = null;
-                        break;
-                    }
+                // No remaining free blocks can be connected to the held block
+                if (freeBlock.start() > heldBlock.start()) {
+                    break;
                 }
 
-                // The held memory could not be merged with a free memory block
-                if (heldBlock != null) {
-                    // Add the held block into the free memory list
-                    freeMemory.add(heldBlock);
+                // The held memory block is connected to a free memory block
+                if (freeBlock.end() == heldBlock.start() || heldBlock.end() == freeBlock.start()) {
+                    // Merge the free block and the held block
+                    Range mergedBlock = new Range(Math.min(freeBlock.start(), heldBlock.start()), Math.max(heldBlock.end(), freeBlock.end()));
+                    
+                    // Replace the free memory block with the two blocks merged
+                    freeMemory.set(j, mergedBlock);
+
+                    // Set held memory block to null, to indicate freed
+                    heldBlock = null;
+                    break;
                 }
             }
+
+            // The held memory could not be merged with a free memory block
+            if (heldBlock != null) {
+                // Add the held block into the free memory list
+                freeMemory.add(heldBlock);
+            }
         }
+
+        System.out.println("Terminated process: PID " + pid);
 
         // Release the semaphore lock
         semaphore.signal();
@@ -171,6 +205,18 @@ public class MemoryManager {
      * Display the current memory layout
      */
     public void printMemory() {
+        // The number of bytes of memory
+        int size = memory.length;
+        int columns = size / MemoryManager.ROWS;
 
+        // Go through each byte, displayed in rows
+        for (int i=0; i<MemoryManager.ROWS; i++) {
+            // Go through each byte within the row
+            for (int j=i * columns; j<i * columns + columns; j++) {
+                System.out.print(" " + memory[j]);
+            }
+            // Print the new line
+            System.out.println();
+        }
     }
 }
