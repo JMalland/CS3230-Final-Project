c31e0a0029a1eab524ad9d461401cca1ceddace0
Jacob Malland <jacob.malland@gmail.com>
2025-05-10 15:49:34 -0500

    Updated Main.java   - Implemented command line   - Removed example methods

--- Main.java
+++ Main.java
@@ -8,26 +8,13 @@ import java.util.Scanner;
  *   GUI Interface
  */
 public class Main {
-    /**
-     * Create a process
-     * @param name The process name
-     */
-    public void create(String name) {
 
-    }
-
-    /**
-     * Allocate memory to a process
-     * @param pid The process ID
-     * @param size The memory to allocate
-     */
-    public void allocate(int pid, int size) {
-
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Create commandline input scanner
         Scanner input = new Scanner(System.in);
+
+        ProcessManager processMgr = new ProcessManager();
+        MemoryManager memoryMgr = new MemoryManager(100);
 
         // Continue running as long as the input has a new line
         while (input.hasNextLine()) {
@@ -36,26 +23,32 @@ public class Main {
 
             // Check the first word against the commands list
             // Ignores invalid casing
-            switch(line.split(" ")[0].toLowerCase()) {
+            switch(line.split(" ", 2)[0].toLowerCase()) {
                 // Create a new process
                 case("create"):
-
+                    processMgr.createProcess(line.split(" ", 2)[1]);
                     break;
                 // Display processes information
                 case("ps"):
-
+                    processMgr.listProcesses();
                     break;
                 // Display process schedule information
                 case("schedule"):
-
+                    processMgr.schedule();
                     break;
                 // Allocate memory to a process
                 case("alloc"):
+                    // Get the PID
+                    int pid = Integer.parseInt(line.split(" ", 4)[1]);
+                    // Get the allocation size
+                    int size = Integer.parseInt(line.split(" ", 4)[2]);
 
+                    // Attempt to allocate memory
+                    memoryMgr.allocate(pid, size);
                     break;
                 // Display memory information
                 case("mem"):
-
+                    memoryMgr.printMemory();
                     break;
                 // Exit the program
                 case("exit"):
