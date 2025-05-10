6323b0088b54786d5b6d92ebdadaf496b1f3fe20
Jacob Malland <jacob.malland@gmail.com>
2025-05-10 12:36:06 -0500

    Created PCB list (queue) for process manager.   - Implemented listProcesses   - Implemented createProcess

--- ProcessManager.java
+++ ProcessManager.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class ProcessManager {
-    
+
+    private ArrayList<PCB> queue;
+    private int nextPid = 0;
+
+    public ProcessManager() {
+        queue = new ArrayList<>();
+    }
 
     /**
      * Create a new process
      * @param name The process name
      */
     public void createProcess(String name) {
-
+        queue.add(new PCB(++ nextPid, name, "READY"));
     }
 
     /**
@@ -21,6 +29,13 @@ public class ProcessManager {
      * Display all processes and their states
      */
     public void listProcesses() {
+        // Clone the queue, and sort by PID
+        ArrayList<PCB> copyQueue = new ArrayList<>(queue);
+        copyQueue.sort((a, b) -> Integer.compare(a.getPid(), b.getPid()));
 
+        // Go through each process, and print its info
+        for (PCB process : copyQueue) {
+            System.out.println("PID " + process.getPid() + " [" + process.getName() + "] : " + process.getState());
+        }
     }
 }
