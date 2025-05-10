f06b8bc2f324d415cefd94f9cd9c05e025fe7348
Jacob Malland <jacob.malland@gmail.com>
2025-05-10 12:10:20 -0500

    Created constructor and getters/setters for PCB.java

--- PCB.java
+++ PCB.java
@@ -2,5 +2,33 @@ public class PCB {
     private int pid;
     private String name;
     private String state; // READY, RUNNING, BLOCKED
-    private boolean active;
+    private boolean active = false;
+
+    /**
+     * Creates a process control block for a given process
+     * @param pid The process ID
+     * @param name The process name
+     * @param state The process state
+     */
+    public PCB(int pid, String name, String state) {
+        this.pid = pid;
+        this.name = name;
+        this.state = state;
+    }
+
+    public int getPid() {
+        return this.pid;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
 }
